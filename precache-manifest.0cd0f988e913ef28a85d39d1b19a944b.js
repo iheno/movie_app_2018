220e46a3ce4117d4624faaecf995af83c9508b6b
Antonio-Jung <antonio_jung@wamazing.jp>
2019-04-26 19:53:40 +0900

    Updates

---
+++ precache-manifest.0cd0f988e913ef28a85d39d1b19a944b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85b5c1c9bf413101e6d7539fd1aecd8",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "9165ead2df52016ce35f",
+    "url": "/movie_app_react/static/css/main.938a141c.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movie_app_react/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "9165ead2df52016ce35f",
+    "url": "/movie_app_react/static/js/main.fc6ced92.chunk.js"
+  },
+  {
+    "revision": "e88e53e1a436651e148c",
+    "url": "/movie_app_react/static/js/runtime~main.21233f46.js"
+  }
+]);
