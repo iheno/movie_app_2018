2232b1c80317ea693ff584ac6f9b238312f2868d
Antonio-Jung <antonio_jung@wamazing.jp>
2019-11-05 15:32:15 +0900

    Updates

--- precache-manifest.53e94a0b57ec39465fd63fc5244cc8af.js
+++ precache-manifest.53e94a0b57ec39465fd63fc5244cc8af.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f85b5c1c9bf413101e6d7539fd1aecd8",
+    "revision": "f4e2d77fecbb5f38ccdcd3c294a8aa07",
     "url": "/movie_app_react/index.html"
   },
   {
-    "revision": "9165ead2df52016ce35f",
+    "revision": "78c3235252b86e30a09d",
     "url": "/movie_app_react/static/css/main.938a141c.chunk.css"
   },
   {
-    "revision": "5cdb4062c6dfdd077298",
-    "url": "/movie_app_react/static/js/2.31ebf22a.chunk.js"
+    "revision": "12db2a3663f9f2c6298a",
+    "url": "/movie_app_react/static/js/2.86296b70.chunk.js"
   },
   {
-    "revision": "9165ead2df52016ce35f",
-    "url": "/movie_app_react/static/js/main.fc6ced92.chunk.js"
+    "revision": "78c3235252b86e30a09d",
+    "url": "/movie_app_react/static/js/main.158ee002.chunk.js"
   },
   {
     "revision": "e88e53e1a436651e148c",
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_react/precache-manifest.0cd0f988e913ef28a85d39d1b19a944b.js"
+  "/movie_app_react/precache-manifest.53e94a0b57ec39465fd63fc5244cc8af.js"
 );
 
 self.addEventListener('message', (event) => {
